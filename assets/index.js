40e976f3e3972b05d1ffbc8ac5522fecf1d6c291
Leonardo Espinoza <leoespinozidk@gmail.com>
2020-02-26 15:40:02 -0600

    aprendiendo this, call, apply y bind

--- assets/index.js
+++ assets/index.js
@@ -4,7 +4,12 @@ import AutoPlay from './plugins/AutoPlay.js'
 const video = document.querySelector("video")
 const button_play = document.querySelector("#button_play")
 const button_mute = document.querySelector("#button_mute")
-const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] })
+const player = new MediaPlayer({ 
+  el: video, 
+  plugins: [
+  // new AutoPlay()] })
+  ]
+})
 
 button_play.onclick = () => {
   if (player.isPaused()){
